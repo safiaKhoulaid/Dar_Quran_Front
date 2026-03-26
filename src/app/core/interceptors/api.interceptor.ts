f90980f9c3f8a506834cb45764d91f7122ed6362
safia-khoulaid <safiakhoulaid11@gmail.com>
2026-03-26 09:06:32 +0000

    fix: correct formatting in api.interceptor.ts for consistent code style

--- src/app/core/interceptors/api.interceptor.ts
+++ src/app/core/interceptors/api.interceptor.ts
@@ -1,9 +1,9 @@
 import { HttpInterceptorFn } from '@angular/common/http';
-import {environment} from '@env/environments';
+import { environment } from '@env/environments';
 
 export const apiInterceptor: HttpInterceptorFn = (req, next) => {
   const apiReq = req.clone({
-    url: `${environment.apiUrl}${req.url}`
+    url: `${environment.apiUrl}${req.url}`,
   });
 
   return next(apiReq);
